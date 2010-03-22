54f9865238fb2ca66b2fa3e3693e359fa2f6945c
piero.contezini <piero.contezini@ab0a4038-8deb-11de-8988-25e8a898b690>
2010-03-22 13:10:33 +0000

    exceptions

---
+++ trunk/beersoap/BeerSoapExceptions.h
@@ -0,0 +1,15 @@
+/*
+ *  BeerSoapExceptions.h
+ *  beersoap
+ *
+ *  Created by Piero Contezini on 02/12/09.
+ *  Copyright 2009 Informant. All rights reserved.
+ *
+ */
+
+enum BeerSoapException {
+	BeerSoapAllocError,
+	BeerSoapInvalidEndpoint,
+	BeerSoapInvalidXML,
+	BeerSoapInvalidWSDL
+};
